7d71176127cf5fc112f57a6bdf1b2db934095edc
chandrabiji2020 <chandra.java04@gmail.com>
2021-02-03 13:22:24 +0530

    first version

---
+++ Demo/src/main/java/com/shiva/Demo/App.java
@@ -0,0 +1,14 @@
+package com.shiva.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    System.out.println("Welcome to first Git Hub Practice");
+    }
+}
